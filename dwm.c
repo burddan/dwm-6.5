c0fca24cbe88664d03ea3ec6715f002dd6dda9da
burddan <faithsteam8818@gmail.com>
2025-03-05 17:02:59 +0000

    ajustes

--- dwm.c
+++ dwm.c
@@ -324,7 +324,7 @@ static void getclassname(Client *c);
 
 /* variables */
 static const char autostartblocksh[] = "autostart_blocking.sh";
-static const char autostartsh[] = "autostart.sh";
+static const char autostartsh[] = "scripts/autostart.sh";
 static const char dwmdir[] = "dwm";
 static const char localshare[] = ".local/share";
 static Systray *systray = NULL;
